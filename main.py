74cb0b276b2266038f810d8c576cde1c8b06885a
Usersname228 <tsharafullin@yandex.ru>
2024-05-16 21:15:18 +0300

    создоавли)

--- main.py
+++ main.py
@@ -10,14 +10,82 @@ class DataBase:
     def __create_table(self):
         sql = self.connect_db()
         sql['cursor'].execute('''
-            
+            CREATE TABLE IF NOT EXISTS users (
+            id                 INTEGER          PRIMARY KEY AUTOINCREMENT,
+            id_telegram         INTEGER          NOT NULL UNIQUE,
+            username           TEXT,
+            first_name         TEXT,
+            last_name          TEXT,
+            date_registration   DATE,
+            access             BOOLEAN          DEFAULT 1
+            )
+        ''')
+        # взять айди от id и переместить в кудато
+    
+        sql['cursor'].execute('''
+            CREATE TABLE IF NOT EXISTS messages (
+            id                 INTEGER          PRIMARY KEY AUTOINCREMENT,
+            id_user            INTEGER          NOT NULL,
+            massege_id         INTEGER          NOT NULL,
+            message_taxt       TEXT             NOT NULL,
+            date_send          DATE,
+            status             BOOLEAN          DEFAULT 0 CHECK(status IN (0, 1)),
+                              
+                FOREIGN KEY (id_user) REFERENCES users(id) 
+            )
         ''')
         self.close(sql['cursor'], sql['connect'])
-        
+
     def connect_db(self):
         with sqlite3.connect(self.db_name) as connect:
             cursor = connect.cursor()
         return{"connect":connect, "cursor":cursor}
+    
+    def check_user(self, user_id):
+        sql = self.connect_db()
+        sql['cursor'].execute('''
+        SELECT * FROM users WHERE id_telegram = ?
+        ''', (user_id, ))
+        info_users = sql['cursor'].fetchone()
+
+        self.close(sql['cursor'], sql['connect'])
+        if info_users is None:
+            return {
+                'status': False
+            }
+        return{
+            'status': True,
+            'info_users': info_users
+        }
+    def create_user(self, message: dict):
+        sql = self.connect_db()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        sql['cursor'].execute('''
+            INSERT INTO users(
+                id_telegram, username, first_name, last_name, date_registration
+            ) VALUES (?, ?, ?, ?, ?)
+        ''', (
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+            date
+        ))
+        sql['connect'].commit()
+        self.close(sql['cursor'], sql['connect'])
+
+    def insert_massage(self, message:dict):
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        sql = self.connect_db()
+        sql['cursor'].execute('''
+            INSERT INTO message(
+                id_user, message_id, message_text, date_send
+            ) VALUES (?, ?, ?, ?)
+        ''', (
+        
+        ))
+        self.close(sql['cursor'], sql['connect'])
+
     def close(self, cursor, connect):
         cursor.close()
         connect.close()
@@ -31,17 +99,23 @@ class TelegramBot(DataBase):
         
         @self.bot.message_handler(commands=['start'])
         def start(message):
-            # print(message)
+            text = ""
+            if self.check_user(message.from_user.id)['status']:
+                text += f"С возращение! {message.from_user.first_name}"
+            else:
+                self.create_user(message)
+                text += f"Добро пожаловать, {message.from_user.first_name}"
+
             self.bot.send_message(
                 message.chat.id,
-                f"Добро пожаловать, {message.from_user.first_name}\n"
-                f"Ваш id: {message.from_user.id}\n"
+                text
             )
+
         @self.bot.message_handler(func=lambda message: True)
         def echo_all(message):
             self.bot.reply_to(
                 message,
-                "Не понимаю...(Я не обучен!)"
+                "Сообщение отправлено хозяину!"
             )
             self.bot.delete_message(
                 chat_id=message.chat.id,
